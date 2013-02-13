7e998aeda6f94bf641a740c089374048a38afc68
Omwah <omwah@oyeh.org>
2013-02-13 09:53:57 -0800

    Initial commit with copy of classes from SDFEconomy @ 87fe495e. Remove dependency on Vault Permissions.

---
+++ README.md
@@ -0,0 +1,22 @@
+OmCommands
+======
+
+This is a project meant to decouple the classes I use in my Bukkit plugins from any one plugin. This project can be compiled into a jar that can be used as a dependency for a plugin. Alternatively it could be included in the plugin's source using git submodules.
+
+These classes originate from some I pulled out of:
+https://github.com/mung3r/ecoCreature
+Which I believe were themselves pulled out of one of DThielke's plugins:
+http://dev.bukkit.org/profiles/DThielke/
+
+I made additional improvements that helped improved the reusability of the classes as I was using them.
+
+Compilation
+-----------
+
+This plugin has a Maven 3 pom.xml and uses Maven to compile. Dependencies are 
+therefore managed by Maven. You should be able to build it with Maven by running
+
+    mvn package
+
+a jar will be generated in the target folder. For those unfa1milliar with Maven
+it is a build system, see http://maven.apache.org/ for more information.
---
+++ src/main/java/com/github/omwah/omcommands/PluginCommand.java
@@ -0,0 +1,40 @@
+package com.github.omwah.OmCommands;
+
+import org.bukkit.command.CommandSender;
+
+/*
+ * Interface that this plugin's commmands implement. 
+ * Named PluginCommand to avoid name clashing with org.bukkit.command.Command
+ */
+
+public interface PluginCommand {
+
+    public void cancelInteraction(CommandSender executor);
+
+    public boolean execute(CommandHandler handler, CommandSender executor, String label, String identifier, String[] args);
+
+    public String getDescription();
+
+    public String[] getIdentifiers();
+
+    public int getMaxArguments();
+
+    public int getMinArguments();
+
+    public String getName();
+
+    public String[] getNotes();
+
+    public String getPermission();
+
+    public String getUsage(String identifier);
+
+    public boolean isIdentifier(CommandSender executor, String input);
+
+    public boolean isInProgress(CommandSender executor);
+
+    public boolean isInteractive();
+
+    public boolean isShownOnHelpMenu();
+
+}
