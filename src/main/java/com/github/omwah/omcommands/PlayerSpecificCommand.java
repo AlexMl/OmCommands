8616f12d9e8a3e33f8f6f93eac46657f1dbfd128
AlexMl <alexandermlinzk@gmail.com>
2014-06-09 14:33:37 +0200

    Updated to UUIDs

--- src/main/java/com/github/omwah/omcommands/PlayerSpecificCommand.java
+++ src/main/java/com/github/omwah/omcommands/PlayerSpecificCommand.java
@@ -1,14 +1,21 @@
+/**
+ * Modified on Jun 9 2014 by AlexMl.
+ * - Updated to UUID to make sure we get no problems with Minecraft 1.8. * 
+ */
 package com.github.omwah.omcommands;
 
 import java.util.ResourceBundle;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 /*
  * Simply adds helper functionality for commands that deal with specific players
  */
-public abstract class PlayerSpecificCommand extends TranslatedCommand
-{
+public abstract class PlayerSpecificCommand extends TranslatedCommand {
+	
     public PlayerSpecificCommand(String name) {
         super(name);
     }
@@ -18,19 +25,20 @@ public abstract class PlayerSpecificCommand extends TranslatedCommand
     }
     
     /*
-     * Gets a player name either as the Sender if it is a Player ir from the arguments
+     * Gets a player uuid either as the Sender if it is a Player or from the arguments
      * if it is supplied there. Returns null if there is a problem, which will be reported
      * to the sender.
      */
-    protected String getDestPlayer(CommandHandler handler, CommandSender sender, String[] args, int playerIndex) {
+    @SuppressWarnings("deprecation")
+	protected UUID getDestPlayer(CommandHandler handler, CommandSender sender, String[] args, int playerIndex) {
 
         // Make sure we are at console or sender has sufficient privileges
         // if a owner name is specified
-        String player_name = null;
+        UUID player_uuid = null;
         if (playerIndex >= 0 && args.length > playerIndex && handler.hasAdminPermission(sender)) {
             
             // Op or admin creating a bank account for another player
-            player_name = args[playerIndex];
+            player_uuid = Bukkit.getPlayer(args[playerIndex]).getUniqueId();
             
         } else if (sender instanceof Player) {
             // Player is sender, make sure they only try and access their own account when a player name
@@ -44,7 +52,7 @@ public abstract class PlayerSpecificCommand extends TranslatedCommand
 
             } else {
                 // Be extra careful that we don't entirely trust the string they sent
-                player_name = ((Player) sender).getName();
+                player_uuid = ((Player) sender).getUniqueId();
             }
 
         } else {
@@ -53,6 +61,6 @@ public abstract class PlayerSpecificCommand extends TranslatedCommand
             return null;
         }
 
-        return player_name;
+        return player_uuid;
      }
 }
